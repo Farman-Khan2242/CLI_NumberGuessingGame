6c69c233a118c8d706ecc1a3e67f2acc3a896a65
FarmanKhan2242 <farman03170218621@gmail.com>
2024-04-24 11:29:14 +0500

    Adding loof

--- index.ts
+++ index.ts
@@ -1,24 +1,29 @@
 #! /usr/bin/env node 
 
 import inquirer from "inquirer";
-
+let condition = true
 let randomNumber = Math.floor(Math.random() * 6 + 1);
-// console.log(randomNumber);if we show the random number than uncomment this line
+// console.log(randomNumber);  //if we show the random number than uncomment this line
 console.log("Welcome to Number guessing Game:")
-const answer = await inquirer.prompt([
-    {
-        name: "Userguessnumber",
-        type: "number",
-        message: "Please guess a number between 1-6: "
+while(condition) {
+    let answer = await inquirer.prompt([
+        {
+            name: "Userguessnumber",
+            type: "number",
+            message: "Please guess a number between 1-6: "
+        }
+    ])
+    if (answer.Userguessnumber === randomNumber){
+        console.log("Congratulation You Guessed Right Number")
+     condition = false
     }
-])
-if (answer.Userguessnumber === randomNumber){
-    console.log("Congratulation! you guessed right number");
+    else if (answer.Userguessnumber > 6) {
+        console.log("Please select Valid Number")
+    }
+    else {
+        console.log("Oops! You guessed wrong number quess again:");
+    }
 }
-else {
-    console.log("Oops! You guessed wrong number quess again:");
-}
-
 
 
 
